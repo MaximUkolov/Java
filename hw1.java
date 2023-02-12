6cca4f26fd5a37a04b412d783d0449339ca58ddd
Max <0665232@mail.ru>
2023-02-13 00:01:56 +0300

    remake 1 task

--- hw1.java
+++ hw1.java
@@ -4,24 +4,27 @@
 
 
 
-// import java.util.Scanner;
-// public class hw1 {
-//     public static void main(String[] args) {
-//         Scanner scanner = new Scanner(System.in);
-//         System.out.print("Введите число n: \n");
-//         int n = scanner.nextInt();
-//         int sum = 0;
+import java.util.Scanner;
+public class hw1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число n: \n");
+        int n = scanner.nextInt();
+        int sum = 0;
+        int mult = 1;
         
-//         for (int i = 1; i <= n; i++) {
-//             sum = sum + i;
-//         }
-//         System.out.println("Сумма n натуральных чисел: " + sum);
-//     }
-// }
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+            mult = mult * i;
+        }
+        System.out.println("Сумма n натуральных чисел: " + sum);
+        System.out.println("Произведение n натуральных чисел: " + mult);
+    }
+}
 
 
 
-// Числа Фибоначчи
+// Числа Фибоначчи    - для себя
 
 // import java.util.Scanner;
 // public class hw1 {
@@ -50,32 +53,32 @@
 // Реализовать простой калькулятор Введите число 2 Введите операцию 1 + 2 - 3 * 4 
 // Введите число 2 2 + 2 = 4
 
-import java.util.Scanner;
-public class hw1{
-    public static void main(String[] args) {
-        double num1;
-        double num2;
-        double res;
-        char op;
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Введите два числа: ");
-        num1 = reader.nextDouble();
-        num2 = reader.nextDouble();
-        System.out.print("\nВведите действие (+, -, *, /): ");
-        op = reader.next().charAt(0);
-        switch(op) {                                                            //Сравниваем переменную со списком значений
-           case '+': res = num1 + num2;
-              break;
-           case '-': res = num1 - num2;
-              break;
-           case '*': res = num1 * num2;
-              break;
-           case '/': res = num1 / num2;
-              break;
-           default:  System.out.printf("Ошибка, введите верное действие");
-              return;
-        }
-        System.out.print("\nОтвет:\n");
-        System.out.printf(num1 + " " + op + " " + num2 + " = " + "%.2f",res);     //%.2f - 2 знака после запятой
-     }
-  }
+// import java.util.Scanner;
+// public class hw1{
+//     public static void main(String[] args) {
+//         double num1;
+//         double num2;
+//         double res;
+//         char op;
+//         Scanner reader = new Scanner(System.in);
+//         System.out.print("Введите два числа: ");
+//         num1 = reader.nextDouble();
+//         num2 = reader.nextDouble();
+//         System.out.print("\nВведите действие (+, -, *, /): ");
+//         op = reader.next().charAt(0);
+//         switch(op) {                                                            //Сравниваем переменную со списком значений
+//            case '+': res = num1 + num2;
+//               break;
+//            case '-': res = num1 - num2;
+//               break;
+//            case '*': res = num1 * num2;
+//               break;
+//            case '/': res = num1 / num2;
+//               break;
+//            default:  System.out.printf("Ошибка, введите верное действие");
+//               return;
+//         }
+//         System.out.print("\nОтвет:\n");
+//         System.out.printf(num1 + " " + op + " " + num2 + " = " + "%.2f",res);     //%.2f - 2 знака после запятой
+//      }
+//   }
