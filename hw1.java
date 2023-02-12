56e43572683d98fcd4179d89fcab64082fc1e2aa
Max <0665232@mail.ru>
2023-02-12 18:42:38 +0300

    feb done

--- hw1.java
+++ hw1.java
@@ -1,16 +1,43 @@
+
+
 // Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
 
-import java.util.Scanner;
-public class hw1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите число n: \n");
-        int n = scanner.nextInt();
-        int sum = 0;
+
+
+// import java.util.Scanner;
+// public class hw1 {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.print("Введите число n: \n");
+//         int n = scanner.nextInt();
+//         int sum = 0;
         
-        for (int i = 1; i <= n; i++) {
-            sum = sum + i;
-        }
-        System.out.println("Сумма n натуральных чисел: " + sum);
-    }
-}
+//         for (int i = 1; i <= n; i++) {
+//             sum = sum + i;
+//         }
+//         System.out.println("Сумма n натуральных чисел: " + sum);
+//     }
+// }
+
+
+
+// Числа Фибоначчи
+
+// import java.util.Scanner;
+// public class hw1 {
+// /**
+//  * @param args
+//  */
+// public static void main(String[] args) {
+//     Scanner scanner = new Scanner(System.in);
+//         System.out.print("Введите число n: ");
+//         int n = scanner.nextInt();
+//         int[] arr = new int[n];
+//         arr[0] = 0;
+//         arr[1] = 1;
+//         for (int i = 2; i < arr.length; i++) {
+//         arr[i] = arr[i - 1] + arr[i - 2];
+//         System.out.println(arr[i]);
+//           }
+// }
+// }
